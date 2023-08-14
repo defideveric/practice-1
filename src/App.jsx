17f1fc941977a416e94e40772ba8cd0b46ee635f
erichudson <defideveric@gmail.com>
2023-08-14 18:19:07 -0400

    react props

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greet from './components/Greet'
+
+const App = () => {
+
+  return (
+   <div>
+      <Greet name='Maxine'/>
+      <Greet name='Charlie'/>
+      <Greet name='Mike'/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Greet.jsx
@@ -0,0 +1,13 @@
+
+const Greet = (props) => {
+    console.log(props)
+  return (
+    <div>
+        <h1>
+            Hello {props.name}
+        </h1>
+    </div>
+  )
+}
+
+export default Greet;
