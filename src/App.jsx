50b02ffc2a31c3d0201b52dbef2bafd207eb17ba
erichudson <defideveric@gmail.com>
2023-08-14 18:24:42 -0400

    react children props

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ const App = () => {
 
   return (
    <div>
-      <Greet name='Maxine'/>
-      <Greet name='Charlie'/>
-      <Greet name='Mike'/>
+      <Greet name='Maxine' heroName='girlfriend'>
+        <p>This is a children prop</p>
+      </Greet>
+      <Greet name='Charlie' heroName='him'>
+        <p>This is a second child prop</p>
+      </Greet>
+      <Greet name='Mike' heroName='himHim'/>
    </div>
   )
 }
--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -4,8 +4,9 @@ const Greet = (props) => {
   return (
     <div>
         <h1>
-            Hello {props.name}
+            Hello {props.name} a.k.a {props.heroName}
         </h1>
+        {props.children}
     </div>
   )
 }
