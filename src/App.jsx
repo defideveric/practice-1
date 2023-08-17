1fa7b3b5cd775032d44116ad28701b391e7cfac2
erichudson <defideveric@gmail.com>
2023-08-17 10:22:38 -0400

    destructuring props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import HookCounter from './components/Hook Counter';
+import Greet from './components/Greet';
 
 const App = () => {
 
   return (
    <div>
-     <HookCounter/>
+    <Greet name ="Eric" heroName="eBreezy"/>
+    <HookCounter/>
    </div>
   )
 }
--- src/components/Hook Counter.jsx
+++ src/components/Hook Counter.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 const HookCounter = () => {
     const [count, setCount] = useState(0)
+    
   return (
     <div>
-        <button onClick={ () => setCount(count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
             Count {count}
         </button>
     </div>
